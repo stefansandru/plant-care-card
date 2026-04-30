42ea60ebbd005296de3e570756d5c14c4ecdd98b
stefansandru <stefansandru04@gmail.com>
2026-04-30 09:39:37 +0300

    env

--- app/run_gunicorn.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gunicorn -w 2 -b :8080 -t 300 -k uvicorn.workers.UvicornWorker --log-config log.ini --reload main:app
--- app/run_uvicorn.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-uvicorn main:app --workers 2 --port 8080 --log-config log.ini --reload
